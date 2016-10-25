184021c26b474038b80373006a3fca6999b3a9b7
zhengjiong <zhengjiong2006@gmail.com>
2016-10-25 18:17:01 +0800

    add click

--- app/src/main/java/com/zj/example/viewdraghelper/Demo3Activity.java
+++ app/src/main/java/com/zj/example/viewdraghelper/Demo3Activity.java
@@ -3,6 +3,9 @@ package com.zj.example.viewdraghelper;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by zj on 2016/9/20.
@@ -13,5 +16,17 @@ public class Demo3Activity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vdh_layout);
+
+        TextView tv2 = (TextView) findViewById(R.id.tv_2);
+
+        tv2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Demo3Activity.this, "click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
+
 }
